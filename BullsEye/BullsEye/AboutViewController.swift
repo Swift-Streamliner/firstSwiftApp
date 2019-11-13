0efb87696246074eb1e8eda23498b779a6c9ab8a
thisisstreamliner <thisisstreamliner@gmail.com>
2019-11-13 13:06:52 +0200

    adding extra screen

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Tiger Mei on 13.11.2019.
+//  Copyright © 2019 Tiger Mei. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
